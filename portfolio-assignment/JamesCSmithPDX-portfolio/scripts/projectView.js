ed45fa6b565e497a0c354162fe84d033f1c8e69c
JamesCSmithPDX <james.c.smithpdx@gmail.com>
2016-05-26 18:38:57 -0700

    IIFE and functional programming

--- portfolio-assignment/JamesCSmithPDX-portfolio/scripts/projectView.js
+++ portfolio-assignment/JamesCSmithPDX-portfolio/scripts/projectView.js
@@ -1,55 +1,54 @@
-var projectView = {};
+(function(module) {
 
-// create tab views of content in #projects and #about
-projectView.handleMainNav = function() {
-  $('.mainNav').on('click', 'li', function() {
-    $('.jumbotron').fadeTo(5000, 1);
-    $('.page-content').hide();
-    $('main').find('[id="'+$(this).attr('data-section')+'"]').fadeIn(5000);
-    projectView.scroll(this);
-  });
-};
+  var projectView = {};
 
-// more and shrink function
-projectView.createTeaser = function() {
-  $('.projDescription *:nth-child(n)').hide();
-  $('.projDescription *:nth-child(1)').show();
-  $('.shrink').hide();
-  $('.read-on').on('click', function(event) {
-    event.preventDefault();
-    $(this).prev().find('*:nth-child(n)').slideDown(1500);
-    $(this).hide();
-    $(this).next().show();
-  });
-  $('.shrink').on('click', function(e) {
-    event.preventDefault();
+  // create tab views of content in #projects and #about
+  projectView.handleMainNav = function() {
+    $('.mainNav').on('click', 'li', function() {
+      $('.jumbotron').fadeTo(5000, 1);
+      $('.page-content').hide();
+      $('main').find('[id="'+$(this).attr('data-section')+'"]').fadeIn(5000);
+      projectView.scroll(this);
+    });
+  };
+
+  // more and shrink function
+  projectView.createTeaser = function() {
     $('.projDescription *:nth-child(n)').hide();
     $('.projDescription *:nth-child(1)').show();
-    $(this).hide();
-    $(this).prev().show();
-  });
-};
-
-projectView.scroll = function(tease) {
-  $('html, body').animate({
-    scrollTop: parseInt($(tease).offset().top)
-  }, 2000);
-};
-
-var byRow = $('body').hasClass('test-rows');
-
-//call the functions
-projectView.initIndexPage = function(){
-  Project.all.forEach(function(a){
-    $('#projects').append(a.toHtml());
-  });
-  $('.page-content').hide();
-  $('.jumbotron').fadeTo(15000, 0.0);
-  projectView.handleMainNav();
-  projectView.createTeaser();
-  $('#projects').each(function() {
-    $(this).children('.sameHeight').matchHeight({
-      byRow: byRow
+    $('.shrink').hide();
+    $('.read-on').on('click', function(event) {
+      event.preventDefault();
+      $(this).prev().find('*:nth-child(n)').slideDown(1500);
+      $(this).hide();
+      $(this).next().show();
     });
-  });
-};
+    $('.shrink').on('click', function(e) {
+      event.preventDefault();
+      $('.projDescription *:nth-child(n)').hide();
+      $('.projDescription *:nth-child(1)').show();
+      $(this).hide();
+      $(this).prev().show();
+    });
+  };
+
+  projectView.scroll = function(tease) {
+    $('html, body').animate({
+      scrollTop: parseInt($(tease).offset().top)
+    }, 2000);
+  };
+
+  var byRow = $('body').hasClass('test-rows');
+
+  //call the functions
+  projectView.initIndexPage = function(){
+    Project.all.forEach(function(a){
+      $('#projects').append(a.toHtml());
+    });
+    $('.page-content').hide();
+    $('.jumbotron').fadeTo(15000, 0.0);
+    projectView.handleMainNav();
+    projectView.createTeaser();
+  };
+  module.projectView = projectView;
+})(window);
